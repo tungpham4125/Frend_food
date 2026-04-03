a5529c2cf65855e21b50865a4507ffd8e2f88b96
CodeWithAnn <tronganfwork@gmail.com>
2026-04-04 04:57:06 +0700

    ggg

--- backend/routes/promotionRoutes.js
+++ backend/routes/promotionRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { getPromotions, createPromotion, applyPromotion } = require('../controllers/promotionController');
+const { getPromotions, createPromotion, applyPromotion, updatePromotion, deletePromotion } = require('../controllers/promotionController');
 const { protect, adminOrStaff } = require('../middleware/authMiddleware');
 
 const router = express.Router();
@@ -7,5 +7,7 @@ const router = express.Router();
 router.get('/', protect, getPromotions);
 router.post('/', protect, adminOrStaff, createPromotion);
 router.post('/apply', protect, applyPromotion);
+router.put('/:id', protect, adminOrStaff, updatePromotion);
+router.delete('/:id', protect, adminOrStaff, deletePromotion);
 
 module.exports = router;
